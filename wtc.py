683f4e7a4a2ff3a9aedd779e2273381f49fc4c09
Christoph Sieber <Christoph.Sieber@telekom.de>
2024-12-11 13:42:45 +0100

    code cleanup
    - apply some pylint recommendations
    - remove duplicate implementations of things which are already in builtins

--- wtc.py
+++ wtc.py
@@ -4,19 +4,20 @@
 This script shows the last notifications from multiple icinga instances
 """
 
-import requests
-from requests.auth import HTTPBasicAuth
 import json
 import typing
-from sys import exit, stdin
 import time
-import select
 from datetime import datetime
 from getpass import getpass, getuser
-import configargparse
 import subprocess
-from re import compile, match
-from colorama import Fore, Style, ansi
+import re
+import sys
+
+import requests
+from requests.auth import HTTPBasicAuth
+import requests.auth
+import configargparse
+from colorama import ansi
 from rich.console import Console
 from rich.table import Table, box
 
@@ -25,9 +26,9 @@ def regex_parse(arg_value):
     checks if the argument is a regex and compiles it - required for input validation
     """
     try:
-        contact_filter = compile(arg_value)
-    except:
-        raise configargparse.ArgumentTypeError("invalid regex")
+        contact_filter = re.compile(arg_value)
+    except Exception as e:
+        raise configargparse.ArgumentTypeError("invalid regex") from e
     return contact_filter
 
 def generate_url(
@@ -38,9 +39,9 @@ def generate_url(
     generates the URL to the check in the webinterface
     """
     if service is None:
-        url = f"{instance}/dashboard#!/monitoring/host/show?host={host}"
+        url = f"{instance}/monitoring/host/show?host={host}"
     else:
-        url = f"{instance}/dashboard#!/monitoring/service/show?host={host}&service={service}"
+        url = f"{instance}/monitoring/service/show?host={host}&service={service}"
     return url
 
 def get_recovered(
@@ -59,34 +60,40 @@ def get_recovered(
             return True
     return False
 
-def get_instance_notifications(instance: str):
+def get_instance_notifications(instance: str, headers: dict, auth, command_args):
     """
     load the notifications of the supplied icinga instance
     """
-    response = requests.request(
-        "GET",
-        f"{instance}/monitoring/list/notifications?notification_timestamp>={args.lookback}",
-        headers=headers,
-        auth=icinga_auth
-    )
     try:
-        output = json.loads(response.text)
-    except json.decoder.JSONDecodeError:
-        print(f"error decoding json from {instance}. Login error?")
-        exit(1)
+        # get recent notifications
+        get_notifications = requests.request(
+            "GET",
+            f"{instance}/monitoring/list/notifications?notification_timestamp>={command_args.lookback}",
+            headers=headers,
+            auth=auth,
+            timeout=command_args.timeout
+        )
 
-    # get recently recovered services
-    recover_req = requests.request(
-        "GET",
-        f"{instance}/monitoring/list/services?service_state=0&limit=500&sort=service_last_state_change&dir=desc",
-        headers=headers,
-        auth=icinga_auth
-    )
+        # get recently recovered services
+        recover_req = requests.request(
+            "GET",
+            f"{instance}/monitoring/list/services?service_state=0&limit=500&sort=service_last_state_change&dir=desc",
+            headers=headers,
+            auth=auth,
+            timeout=command_args.timeout
+        )
+        get_notifications.raise_for_status()
+        recover_req.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print('error requesting data from icinga:', e)
+        sys.exit(1)
+
     try:
+        output = json.loads(get_notifications.text)
         recovered_list = json.loads(recover_req.text)
-    except json.decoder.JSONDecodeError:
-        print(f"error decoding recover json from {instance}. Login error?")
-        exit(1)
+    except json.decoder.JSONDecodeError as e:
+        print(f"error decoding json from {instance}. Login error?", e)
+        sys.exit(1)
 
     # add additional information to notification list output
     for row in output:
@@ -103,20 +110,18 @@ def get_instance_notifications(instance: str):
         row.update({"recovered": recovered })
     return output
 
-def sort_by_ts(elem):
-    """helper function to return the timestamp from the element dict for sorting"""
-    return elem.get("notification_timestamp")
-
-def data_of_instances(instances):
+def data_of_instances(auth, _args):
     """fetches data from multiple icinga instances"""
-    returns = []
+    headers = {
+        "Accept": "application/json"
+    }
+    instances = _args.instance
+    icinga_notifications = []
     for instance in instances:
-        icinga_output = get_instance_notifications(
-            instance=instance
-        )
-        returns.extend(icinga_output)
-    returns.sort(key=sort_by_ts, reverse=True)
-    return returns
+        icinga_output = get_instance_notifications(instance, headers, auth, _args)
+        icinga_notifications.extend(icinga_output)
+    icinga_notifications.sort(key=lambda x: x.get('notification_timestamp'), reverse=True)
+    return icinga_notifications
 
 def show_time(ts):
     """icinga returns timestamps as unix timestamp - this produces "readable" time for the output"""
@@ -139,74 +144,75 @@ def recovered_string(recovered):
     else:
         return '[red]False'
 
-def text_output(notifications, limit: int):
+def text_output(notifications, limit: int, console):
     """generates text output from fetched notifications"""
-    counter = 0
     table = Table(box=box.SIMPLE_HEAVY)
-    table.add_column("Number", style="dim", width=5)
-    table.add_column("Timestamp", width=20)
+    table.add_column("#", style="dim", width=2)
+    table.add_column("Timestamp", width=19)
     table.add_column("State")
     table.add_column("Recovered")
     table.add_column("Hostname")
     table.add_column("Service")
 
-    for r in notifications:
+    for counter, r in enumerate(notifications):
         timestamp = show_time(int(r.get("notification_timestamp")))
         hostname = r.get("host_name")
         service = r.get("service_display_name")
         state = state_string(r.get("notification_state"))
         recovered = recovered_string(r.get("recovered"))
 
-        if filter_notification(r):
-            if counter < limit:
-                table.add_row(
-                        f"{counter+1:02d}",
-                        timestamp,
-                        state,
-                        recovered,
-                        hostname,
-                        service
-                    )
-                counter += 1
-
-            else:
-                break
+        if counter < limit:
+            table.add_row(
+                f"{counter+1:02d}",
+                timestamp,
+                state,
+                recovered,
+                hostname,
+                service
+            )
+        else:
+            break
 
     console.print(table)
 
 def filter_notification(notification):
-    if notification.get("notification_contact_name") != None:
+    """
+    Filters notifications for matching the configured regex
+    """
+    if notification.get("notification_contact_name") is not None:
         if args.filter.match(notification.get("notification_contact_name")):
             return True
     return False
 
 def check_input(notifications):
-    print('press enter to refresh or enter a entry number to open the check in the web browser (using xdg-open):')
+    """
+    checks if the user wants to open the icinga page of a specific check
+    """
+    print('press enter to refresh or enter a entry number',
+          'to open the check in the web browser (using xdg-open):')
+
     user_input = input('([0-9]|q)> ')
 
-    if match('[0-9]+', user_input):
-        counter = 0
-        for r in notifications:
-            if filter_notification(r):
-                counter += 1
-                if counter == int(user_input):
-                    url = r.get('url')
-                    break
+    if re.match('[0-9]+', user_input):
+        try:
+            url = notifications[int(user_input)-1]['url']
+        except IndexError:
+            pass
+
         if not args.show_urls:
-            subprocess.run(['xdg-open', url])
+            subprocess.run(['xdg-open', url], check=False)
         else:
             print(url)
             print('copy or open url and press enter to refresh screen')
             input()
     if user_input == 'q':
-        exit(0)
+        sys.exit(0)
 
 if __name__ == "__main__":
-
     p = configargparse.ArgParser(
-    default_config_files=['~/.config/wtc.yml'],
-    config_file_parser_class=configargparse.YAMLConfigFileParser,
-    formatter_class=configargparse.ArgumentDefaultsHelpFormatter,
+        default_config_files=['~/.config/wtc.yml'],
+        config_file_parser_class=configargparse.YAMLConfigFileParser,
+        formatter_class=configargparse.ArgumentDefaultsHelpFormatter,
     )
 
     p.add(
@@ -275,37 +281,38 @@ if __name__ == "__main__":
         help="only output calls once and exit afterwards",
         default=False
     )
+    p.add(
+        "--timeout",
+        "-T",
+        help="timeout for http requests to the icinga instances",
+        type=int,
+        default=30
+    )
 
     args = p.parse_args()
 
-    console = Console()
+    c = Console()
 
 
     # ask for password if it isn't set from the commandline
     if args.password is None:
-        password = getpass(f'enter password for {args.user}:')
+        password = getpass(f'enter password for {args.user}: ')
     else:
         password = args.password
 
     icinga_auth = HTTPBasicAuth(args.user, password)
 
-    headers = {
-        "Accept": "application/json"
-    }
-
     while True:
         try:
-            notifs = data_of_instances(args.instance)
+            notifs = data_of_instances(icinga_auth, args)
             print(ansi.clear_screen())
-            text_output(
-                notifications=notifs,
-                limit=args.limit
-            )
+            notifs = list(filter(filter_notification, notifs))
+            text_output(notifs, args.limit, c)
             if args.watch:
                 time.sleep(args.watch_interval)
             elif args.onetime:
-                exit(0)
+                sys.exit(0)
             else:
                 check_input(notifs)
         except KeyboardInterrupt:
-            exit(0)
+            sys.exit(0)
